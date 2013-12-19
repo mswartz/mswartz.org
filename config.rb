2ac057492013a1f3806887fb1be9ef5d3f32579c
Mike Swartz <mswartz@gmail.com>
2013-12-18 20:35:36 -0500

    adding upbase, structuring stuff

---
+++ config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+relative_assets = true
+add_import_path "bower_components/upbase/components"
+line_comments = false
+output_style = :compact
